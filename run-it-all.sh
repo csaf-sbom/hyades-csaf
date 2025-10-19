6dd24ad36a1ab7ef6a91087fe35e5abfcc4b4b78
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-10-19 12:45:49 +0200

    Remove CSAF view-doc modal and integrate product tree into advisory detail; remove view-details button

--- run-it-all.sh
+++ run-it-all.sh
@@ -15,7 +15,12 @@ git submodule update --init --recursive --remote
 
 # Build hyades modules
 cd $BASEDIR/hyades
-mvn clean package -DskipTests=true
+# Note: we are NOT building native images (yet), but we need to match the
+# tag used in docker-compose.yaml
+mvn clean package\
+  -Dquarkus.container-image.additional-tags=snapshot-native\
+  -Dquarkus.container-image.build=true\
+  -DskipTests=true
 
 # Build hyades-apiserver
 cd $BASEDIR/hyades-apiserver
@@ -23,13 +28,13 @@ mvn install -DskipTests -Dcheckstyle.skip
 
 cd $BASEDIR/hyades-apiserver
 mvn -pl apiserver -am clean package -P quick
-docker build -t ghcr.io/dependencytrack/hyades-apiserver:local -f apiserver/src/main/docker/Dockerfile apiserver
+docker build -t ghcr.io/dependencytrack/hyades-apiserver:snapshot -f apiserver/src/main/docker/Dockerfile apiserver
 
 # Build hyades-frontend
 cd $BASEDIR/hyades-frontend
 npm install
 npm run build
-docker build -f docker/Dockerfile.alpine -t ghcr.io/dependencytrack/hyades-frontend:local .
+docker build -f docker/Dockerfile.alpine -t ghcr.io/dependencytrack/hyades-frontend:snapshot .
 
 # Run docker-compose
 cd $BASEDIR/hyades
